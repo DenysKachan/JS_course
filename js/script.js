09dfe6a7e1a951afef07033aca510fa7b042d131
Denys <denkacha220406@gmail.com>
2021-07-17 15:08:22 +0300

    Objects practice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 "use strict";
 
+let numberOfFilms = 10;
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
+let a = "Dacia",
+    b = 5,
+    c = "Logan",
+    d = 6;
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
